f0bb56f12201d2924281d081b6ea1092bcd01509
BXSoft <jeremiahlowinne@gmail.com>
2025-06-02 11:24:59 +0200

    First Commit

---
+++ src/health/health.controller.ts
@@ -0,0 +1,19 @@
+
+import { Controller, Get } from '@nestjs/common';
+import { HealthCheckService, HttpHealthIndicator, HealthCheck } from '@nestjs/terminus';
+
+@Controller('health')
+export class HealthController {
+  constructor(
+    private health: HealthCheckService,
+    private http: HttpHealthIndicator,
+  ) {}
+  //A remplacer avec health check de la base de données et de rabbit MQ
+  @Get()
+  @HealthCheck()
+  check() {
+    return this.health.check([
+      () => this.http.pingCheck('nestjs-docs', 'https://docs.nestjs.com'),
+    ]);
+  }
+}
