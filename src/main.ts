44142e45e2060e25fb1e388d4fd39af761452c8a
MatNgl <mattheo.naegellen@gmail.com>
2025-06-04 12:36:36 +0200

    Revert "Merge branch 'feature/updatefront'"
    This reverts commit b7234105362e51f9295e71cf9d44098bee14aaf0, reversing
    changes made to 13db31d1e5dca9c24521cb03b267f41e7ad7b585.

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,3 @@
-// src/main.ts
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
@@ -6,31 +5,19 @@ import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // ----------------------------
-  // 1) Autoriser le CORS pour tout appel venant de http://localhost:4000
-  // ----------------------------
-  app.enableCors({
-    origin: 'http://localhost:4000',
-    // si vous avez besoin de cookies ou tokens httpOnly, dé-commentez :
-    // credentials: true,
-  });
-
-  // ----------------------------
-  // 2) Connexion à votre microservice RabbitMQ
-  // ----------------------------
   app.connectMicroservice<MicroserviceOptions>({
     transport: Transport.RMQ,
     options: {
       urls: ['amqp://rabbitmq:5672'],
       queue: 'channel_message',
       queueOptions: { durable: true },
-      noAck: false,
-    },
-  });
+      noAck : false
+    }
+    });
 
-  // Démarre l’écoute du microservice
-  await app.startAllMicroservices();
-  // Démarre l’API NestJS sur le port 3000
+  await app.startAllMicroservices(); 
   await app.listen(3000);
 }
+
+
 bootstrap();
