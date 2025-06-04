ce5042e0a9ad8be6dca41eaf4ac20ede8b0584ac
MatNgl <mattheo.naegellen@gmail.com>
2025-06-04 15:29:48 +0200

    feature/createlogfront: connexion apollo front

--- src/main.ts
+++ src/main.ts
@@ -5,19 +5,24 @@ import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // Autoriser le front (http://localhost:4000) à faire des requêtes CORS
+  app.enableCors({
+    origin: 'http://localhost:4000',
+    // credentials: true,  // à décommenter si vous utilisez des cookies/httpOnly
+  });
+
   app.connectMicroservice<MicroserviceOptions>({
     transport: Transport.RMQ,
     options: {
       urls: ['amqp://rabbitmq:5672'],
       queue: 'channel_message',
       queueOptions: { durable: true },
-      noAck : false
-    }
-    });
+      noAck: false,
+    },
+  });
 
-  await app.startAllMicroservices(); 
+  await app.startAllMicroservices();
   await app.listen(3000);
 }
-
 
 bootstrap();
