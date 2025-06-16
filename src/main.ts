c10d184f214d3c25b8bb276904fdfaaecd3f462b
MatNgl <mattheo.naegellen@gmail.com>
2025-06-16 14:26:36 +0200

    last version

--- src/main.ts
+++ src/main.ts
@@ -5,19 +5,25 @@ import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // 1) Active CORS pour votre front Next.js sur le port 4000
+  app.enableCors({
+    origin: 'http://localhost:4000',
+    credentials: true,
+  });
+
+  // 2) Monte le microservice RabbitMQ
   app.connectMicroservice<MicroserviceOptions>({
     transport: Transport.RMQ,
     options: {
-      urls: ['amqp://rabbitmq:5672'],
-      queue: 'channel_message',
+      urls: [process.env.RABBITMQ_URL!],   // ex: amqp://guest:guest@rabbitmq:5672
+      queue: process.env.RABBITMQ_QUEUE,  // ex: channel_message
       queueOptions: { durable: true },
-      noAck : false
-    }
-    });
+      noAck: false,
+    },
+  });
 
-  await app.startAllMicroservices(); 
+  await app.startAllMicroservices();
   await app.listen(3000);
 }
-
 
 bootstrap();
